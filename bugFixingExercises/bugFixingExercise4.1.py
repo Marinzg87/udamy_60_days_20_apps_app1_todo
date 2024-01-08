ad5c6f31d8eeef190b09e034e225f9e6fdf5fc90
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-01-08 07:12:25 +0100

    Day_5 App_1 ToDos - end the day 5. List, indexing, Tuples.

---
+++ bugFixingExercises/bugFixingExercise4.1.py
@@ -0,0 +1,7 @@
+# before - should print out 'b'
+# elements = ['a', 'b', 'c']
+# print(elements(1))
+
+# after
+elements = ['a', 'b', 'c']
+print(elements[1])
---
+++ bugFixingExercises/bugFixingExercise4.2.py
@@ -0,0 +1,12 @@
+# before - repalce 'b' with 'x'
+elements = ['a', 'b', 'c']
+new = 'x'
+new = elements[1]
+print(elements)
+
+# after
+elements = ['a', 'b', 'c']
+new = 'x'
+elements[1] = new
+print(elements)
+
